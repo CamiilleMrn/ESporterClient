ae565c167f337023051efe447287195c9f25b729
CamiilleMrn <marion_camille@yahoo.com>
2022-11-18 15:00:17 +0100

    tout calendrier ok sauf arbitre

--- Esporter/src/ihm/organisateur/TournoisRendererOrga.java
+++ Esporter/src/ihm/organisateur/TournoisRendererOrga.java
@@ -35,10 +35,13 @@ public class TournoisRendererOrga extends JPanel{
 	private JLabel lbTournamentName = new JLabel();
 	private JButton lbArrowIcon = new JButton();
 	private final JPanel panel = new JPanel();
-	private final Component verticalGlue = Box.createVerticalGlue();
-	private final JLabel lbInscriptionButton = new JLabel();
-	private final Component verticalGlue_1 = Box.createVerticalGlue();
 	private TournoiInfo tournoi;
+	private final Component verticalGlue = Box.createVerticalGlue();
+	private final JPanel panel_1 = 	new JPanel();
+	private final JButton lbModifyButton = new JButton();
+	private final Component verticalGlue_1 = Box.createVerticalGlue();
+	private final JLabel lbSep = new JLabel(" | ");
+	private final JButton btnSupprimer = new JButton("Supprimer");
 	
 	
 	public TournoisRendererOrga (TournoiInfo tournoi) {
@@ -52,17 +55,57 @@ public class TournoisRendererOrga extends JPanel{
 		lbTournamentName.setForeground(MasterFrame.COULEUR_TEXTE);
 		panelText.add(lbTournamentName);
 		add(panelText, BorderLayout.WEST);
+		panel.setBackground(MasterFrame.COULEUR_MASTER_FOND);
+		panelText.add(panel);
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 		
+		panel.add(verticalGlue);
+		
+		panel_1.setBackground(MasterFrame.COULEUR_MASTER_FOND);
+		
+		panel.add(panel_1);
+		panel_1.setLayout(new BoxLayout(panel_1, BoxLayout.X_AXIS));
+		lbModifyButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		lbModifyButton.setText("Modifier");
+		lbModifyButton.setHorizontalAlignment(SwingConstants.CENTER);
+		lbModifyButton.setForeground(MasterFrame.COULEUR_TEXTE);
+		lbModifyButton.setBorder(new CompoundBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(255, 255, 255)), new EmptyBorder(3, 11, 3, 10)));
+		lbModifyButton.setBackground(new Color(96, 96, 96));
+		lbModifyButton.setAlignmentX(0.5f);
+		
+		panel_1.add(lbModifyButton);
+		
+		lbSep.setForeground(MasterFrame.COULEUR_TEXTE);
+		panel_1.add(lbSep);
+		btnSupprimer.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		btnSupprimer.setHorizontalAlignment(SwingConstants.CENTER);
+		btnSupprimer.setForeground(MasterFrame.COULEUR_TEXTE);
+		btnSupprimer.setBorder(new CompoundBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(255, 255, 255)), new EmptyBorder(3, 11, 3, 10)));
+		btnSupprimer.setBackground(new Color(96, 96, 96));
+		btnSupprimer.setAlignmentX(0.5f);
+		panel_1.add(btnSupprimer);
+		
+		panel.add(verticalGlue_1);
+
 		lbArrowIcon.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
 			}
 		});
+		add(lbArrowIcon, BorderLayout.EAST);
+		
+		lbTournamentName.setText(this.tournoi.getNom()+ " - " +this.tournoi.getDateInscription());
 		
 		lbArrowIcon.setBorder(null);
 		lbArrowIcon.setBackground(MasterFrame.COULEUR_MASTER_FOND);
-		add(lbArrowIcon, BorderLayout.EAST);
-		lbTournamentName.setText(this.tournoi.getNom()+" - "+this.tournoi.getDateInscription());
 		try {
 			lbArrowIcon.setIcon(new ImageIcon(ImageIO.read(getClass().getResource("../images/Chevron.png"))));
 		} catch (IOException e) {
@@ -71,5 +114,6 @@ public class TournoisRendererOrga extends JPanel{
 		}
 		
 	}
-	
+		
 }
+	
