b2add4fc4e2e46cfc469fcf01be34e969d4dde0a
CamiilleMrn <marion_camille@yahoo.com>
2022-11-21 08:05:14 +0100

    Calendrier

--- Esporter/src/ihm/TypeMenu.java
+++ Esporter/src/ihm/TypeMenu.java
@@ -7,18 +7,18 @@ public enum TypeMenu {
 	
 	
 	Visiteurs(new boutonMenu[] {new boutonMenu(true, "Accueil",  new ihm.visiteur.Accueil()),
-								new boutonMenu(false, "Calendrier", new ihm.visiteur.Calendrier()),
+								new boutonMenu(false, "Calendrier", new ihm.Calendrier()),
 								new boutonMenu(false, "Classement",  new ihm.Classement()),
 								new boutonMenu(false, "Ecuries",  new ihm.Ecuries())}),
 	
 	Ecuries(new boutonMenu[] {	new boutonMenu(true, "Accueil",  new ihm.ecurie.Accueil()),
-								new boutonMenu(false, "Calendrier",  new ihm.ecurie.Calendrier()),
+								new boutonMenu(false, "Calendrier",  new ihm.Calendrier()),
 								new boutonMenu(false, "Classement",  new ihm.Classement()),
 								new boutonMenu(false, "Ecuries",  new ihm.Ecuries()),
 								new boutonMenu(false, "Gestion des equipes",  new ihm.ecurie.GestionEquipe())}),
 	
 	Joueurs(new boutonMenu[] {	new boutonMenu(true, "Accueil",  new ihm.joueur.Accueil()),
-								new boutonMenu(false, "Calendrier",  new ihm.joueur.Calendrier()),
+								new boutonMenu(false, "Calendrier",  new ihm.Calendrier()),
 								new boutonMenu(false, "Classement",  new ihm.Classement()),
 								new boutonMenu(false, "Ecuries",  new ihm.Ecuries())}),
 	
@@ -28,7 +28,7 @@ public enum TypeMenu {
 									new boutonMenu(false, "Ecuries",  new ihm.Ecuries())}),
 	
 	Arbitres(new boutonMenu[] {	new boutonMenu(true, "Accueil",  new ihm.visiteur.Accueil()),
-								new boutonMenu(false, "Calendrier",  new ihm.visiteur.Calendrier()),
+								new boutonMenu(false, "Calendrier",  new ihm.Calendrier()),
 								new boutonMenu(false, "Classement",  new ihm.Classement()),
 								new boutonMenu(false, "Ecuries",  new ihm.Ecuries())});	
 	
